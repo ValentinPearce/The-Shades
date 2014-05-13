9f991e6d06accef3bddc5959659b85f8b1163d5f
MrDarkmini <pierremahieux@hotmail.fr>
2014-05-13 16:34:17 +0200

    added random Item fonction

---
+++ Items.py
@@ -0,0 +1,29 @@
+import random
+
+items = (
+	{"name" : "une Fée" , "rate" : 4 , "type" : 0 , "Modifier" : 6}
+	{"name" : "un Coeur" , "rate" : 10 , "type" : 0 , "Modifier" : 1}
+	{"name" : "une Galette Saucisse" , "rate" : 9 , "type" : 0 , "Modifier" : 2}
+	{"name" : "des Abats" , "rate" : 10 , "type" : 0 , "Modifier" : 1}
+	{"name" : "des Jelly Babies" , "rate" : 7 , "type" : 0 , "Modifier" : 4}
+	{"name" : "une Fleu de Feu" , "rate" : 7 , "type" : 1 , "Modifier" : 1}
+	{"name" : "un Arc" , "rate" : 7 , "type" : 1 , "Modifier" : 1}
+	{"name" : "une Arbalette" , "rate" : 5 , "type" : 1 , "Modifier" : 3}
+	{"name" : "un Morgenstern" , "rate" : 5 , "type" : 1 , "Modifier" : 3}
+	{"name" : "une Masse" , "rate" : 7 , "type" : 1 , "Modifier" : 2}
+	{"name" : "la Master Sword" , "rate" : 2 , "type" : 1 , "Modifier" : 6}
+	{"name" : "un Lance-Pierre" , "rate" : 7 , "type" : 1 , "Modifier" : 1}
+	{"name" : "une Catapulte" , "rate" : 3 , "type" : 1 , "Modifier" : 5}
+	{"name" : "une Claymore" , "rate" : 5 , "type" : 1 , "Modifier" : 3}
+	{"name" : "un Katana" , "rate" : 6 , "type" : 1 , "Modifier" : 2}
+	{"name" : "un Tromblon" , "rate" : 6 , "type" : 1 , "Modifier" : 2})
+
+
+def addRandom():
+	item = random.randint(1,100)
+	rate = 0
+	index = 0
+	while rate < item:
+		rate += items [index]["rate"]
+		index += 1
+	return items[index]
